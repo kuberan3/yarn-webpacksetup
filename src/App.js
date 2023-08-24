78302117bcecaaa68c0cc8ea0c64dbd456491bf1
kuberan <59096399+kuberan3@users.noreply.github.com>
2023-08-24 18:02:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SampleImage from 'assets/SampleImage.js';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React App</h1>
+ 
+      <SampleImage></SampleImage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/SampleImage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SampleImage() {
+  return <img src="https://img.freepik.com/free-vector/sticker-template-cat-cartoon-character_1308-73786.jpg?w=2000" alt="Icon" />;
+}
+
+export default SampleImage;
